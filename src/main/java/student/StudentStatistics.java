39801b26f08a64211b02e45699e2317389944979
POS_Wiederholungen <junbochen06@gmail.com>
2021-09-21 12:10:56 +0200

    Something news

--- src/main/java/student/StudentStatistics.java
+++ src/main/java/student/StudentStatistics.java
@@ -3,10 +3,12 @@ package student;
 import student.domain.Gender;
 import student.domain.Student;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StudentStatistics {
@@ -15,49 +17,58 @@ public class StudentStatistics {
 
     /**
      * Reads all students from given csv-File.
+     *
      * @param path the file
      * @throws IOException
      */
     public StudentStatistics(Path path) throws IOException {
-        try(Stream<String> lines = Files.lines(path)
-                                        .skip(1)){
-           students=new TreeSet<>();
-            lines.forEach(s -> {
-                students.add(Student.of(s));
-            });
-        }
+        students = Files.lines(path)
+                .skip(1)
+                .map(s -> Student.of(s))
+                .collect(Collectors.toSet());
+
+        //students=students.stream()
+        //         .collect(Collectors.toMap(Student::schoolClass,student -> student));
     }
 
     /**
      * Counts all students of given gender
+     *
      * @param gender the gender
      * @return number of students of the given gender
      */
     public long countGender(Gender gender) {
-        Stream.of(students)
-                .forEach(students1 -> );
-        return 0;
+        return students.stream()
+                .filter(student -> student.gender().equals(gender))
+                .count();
     }
 
     /**
      * Returns all students classes sorted alphabetically.
+     *
      * @return all students classes sorted alphabetically
      */
     public SortedSet<String> getClasses() {
-        return null;
+        return students.stream()
+                .map(student -> student.schoolClass())
+                .sorted()
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 
     /**
      * Returns a map containing the number students of each gender in a given class.
+     *
      * @param schoolClass the class
      * @return count of students of each gender
      */
     public Map<Gender, Long> getGenderCountForClass(String schoolClass) {
+
         return null;
     }
 
     /**
      * Returns all students whose second name contains the given sequence.
+     *
      * @param sequence the sequence to search for. Case sensitive
      * @return students named like %sequence%
      */
@@ -67,7 +78,8 @@ public class StudentStatistics {
 
     /**
      * Finds a student by number and class
-     * @param number students number
+     *
+     * @param number      students number
      * @param schoolClass students class
      * @return the student or Optional.empty if no student matches criteria
      */
@@ -77,6 +89,7 @@ public class StudentStatistics {
 
     /**
      * Returns the student with the longest name.
+     *
      * @return any student whose name contains the highest number of letters in all names
      */
     public Student getStudentWithLongestName() {
@@ -85,6 +98,7 @@ public class StudentStatistics {
 
     /**
      * Returns the most frequently found first names
+     *
      * @param count how many should be returned
      * @return the topX most frequent first names
      */
@@ -94,6 +108,7 @@ public class StudentStatistics {
 
     /**
      * Returns the number of students in each year.
+     *
      * @return count of students of each year
      */
     public Map<Integer, Long> countStudentsByYear() {
